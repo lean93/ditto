f2e909cd31f69fd3da6ecbfc34e360f0dc76604a
lean93 <leandro.rodriguez@despegar.com>
2019-03-26 19:06:37 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-const digitsOnly = /^\d+$/;
+import GenericFilter from './ditto/GenericFilter'
 
-// function to mask digits into US phone format
-function maskUSPhone(phone) {
-  // returning null when getting null, undefined, or an object that is no string as a parameter
-  if (!phone || typeof phone !== 'string') {
-    return null;
-  }
-
-  // returning the untouched value when it contains non-digit chars or when it has a length != 10
-  if (digitsOnly.test(phone) === false || phone.length !== 10) {
-    return phone;
-  }
-
-  // returning the masked value
-  const codeArea = phone.substring(0, 3);
-  const prefix = phone.substring(3, 6);
-  const sufix = phone.substring(6, 10);
-  return `(${codeArea}) ${prefix}-${sufix}`;
-}
-
-export default maskUSPhone;
+export default GenericFilter;
